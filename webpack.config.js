7fce637c5e8f36e0b0fcebc2899f1500c5d0d0e7
Joe Barnes <joebarnes333@gmail.com>
2019-05-31 15:44:53 -0700

    add planet images

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
+
+
 module.exports = {
     mode: 'development',
     entry: './src/main.js',
